daba603088ad71e437d17f99fc4094b775be7f96
Apollo Gonzalez <apollo.gonzo@gmail.com>
2019-09-23 16:16:24 -0500

    Made changes to migration files before pub to Azure

---
+++ MissionControlApp.API/Migrations/20190607213440_AddMissionPlatformv2.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MissionControlApp.API.Migrations
+{
+    public partial class AddMissionPlatformv2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MissionPlatform_Missions_MissionId",
+                table: "MissionPlatform");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MissionPlatform_Platforms_PlatformId",
+                table: "MissionPlatform");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MissionPlatform",
+                table: "MissionPlatform");
+
+            migrationBuilder.RenameTable(
+                name: "MissionPlatform",
+                newName: "MissionPlatforms");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MissionPlatform_PlatformId",
+                table: "MissionPlatforms",
+                newName: "IX_MissionPlatforms_PlatformId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MissionPlatform_MissionId",
+                table: "MissionPlatforms",
+                newName: "IX_MissionPlatforms_MissionId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MissionPlatforms",
+                table: "MissionPlatforms",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MissionPlatforms_Missions_MissionId",
+                table: "MissionPlatforms",
+                column: "MissionId",
+                principalTable: "Missions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MissionPlatforms_Platforms_PlatformId",
+                table: "MissionPlatforms",
+                column: "PlatformId",
+                principalTable: "Platforms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MissionPlatforms_Missions_MissionId",
+                table: "MissionPlatforms");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MissionPlatforms_Platforms_PlatformId",
+                table: "MissionPlatforms");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MissionPlatforms",
+                table: "MissionPlatforms");
+
+            migrationBuilder.RenameTable(
+                name: "MissionPlatforms",
+                newName: "MissionPlatform");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MissionPlatforms_PlatformId",
+                table: "MissionPlatform",
+                newName: "IX_MissionPlatform_PlatformId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MissionPlatforms_MissionId",
+                table: "MissionPlatform",
+                newName: "IX_MissionPlatform_MissionId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MissionPlatform",
+                table: "MissionPlatform",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MissionPlatform_Missions_MissionId",
+                table: "MissionPlatform",
+                column: "MissionId",
+                principalTable: "Missions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MissionPlatform_Platforms_PlatformId",
+                table: "MissionPlatform",
+                column: "PlatformId",
+                principalTable: "Platforms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ MissionControlApp.API/Migrations/20190904212631_MissionStatusTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MissionControlApp.API.Migrations
+{
+    public partial class MissionStatusTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MissionStatus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MissionStatusName = table.Column<string>(nullable: true),
+                    MissionStatusCode = table.Column<string>(nullable: true),
+                    MissionStatusAlias = table.Column<string>(nullable: true),
+                    Sequence = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    MissionStatusType = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTime>(nullable: true),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MissionStatus", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MissionStatus");
+        }
+    }
+}
